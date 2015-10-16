ea867aadbe03e66733793f0ecde704671d624ef5
Natalie K <natalie.kraft@hp.com>
2015-10-16 09:56:13 +0200

    Die Map wird nun in dieser Activity erstellt, notwendig um weiterhin aktuelle Geodaten an Server zu senden und diese auch für die Map zu nutzen

--- meetme-app/src/main/java/de/dhbw/meetme/HelloActivity.java
+++ meetme-app/src/main/java/de/dhbw/meetme/HelloActivity.java
@@ -2,27 +2,27 @@ package de.dhbw.meetme;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Environment;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import org.apache.http.client.methods.HttpPut;
 import org.hello.R;
-
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -31,13 +31,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import android.widget.Toast;
 import com.google.android.gms.maps.*;
@@ -57,22 +51,20 @@ public class HelloActivity extends Activity implements LocationListener, View.On
     private LocationManager locationManager;
     private TextView anzeigeLaenge;
     private TextView anzeigeBreite;
-    private TextView anzeigeHoehe;
     private EditText anzeigeUserlist;
-    private SimpleDateFormat gpxZeitFormat;
 
-    private Button startButton;
-    private Button stoppButton;
-    private Button speichernButton;
     private Button userlistButton;
     private Button clearButton;
     private Button mapButton;
     private boolean datenSammeln;
     private List<Location> positionen;
-    private GoogleMap googleMap;
     private double lat;
     private double lng;
     private boolean geodaten;
+    public boolean maperstellt;
+    private GoogleMap googleMap;
+    private Marker marker;
+    //private String provider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -84,7 +76,6 @@ public class HelloActivity extends Activity implements LocationListener, View.On
         //Das Widget mit dem Steuerelement verbinden
         anzeigeBreite = (TextView) this.findViewById(R.id.textViewBreite);
         anzeigeLaenge = (TextView) this.findViewById(R.id.textViewLaenge);
-        anzeigeHoehe = (TextView) this.findViewById(R.id.textViewHoehe);
         anzeigeUserlist = (EditText) this.findViewById(R.id.textViewUserlist);
 
 
@@ -93,17 +84,6 @@ public class HelloActivity extends Activity implements LocationListener, View.On
             warnungUndBeenden();
         }
 
-        // StartButton
-        startButton = (Button) this.findViewById(R.id.button1);
-        startButton.setOnClickListener(this);
-        // Delete-Button
-        stoppButton = (Button) this.findViewById(R.id.button2);
-        stoppButton.setOnClickListener(this);
-        stoppButton.setEnabled(false);
-        // Speichern-Button
-        speichernButton = (Button) this.findViewById(R.id.button3);
-        speichernButton.setOnClickListener(this);
-        speichernButton.setEnabled(false);
         //Userlist-Button
         userlistButton = (Button) this.findViewById(R.id.button4);
         userlistButton.setOnClickListener(this);
@@ -116,10 +96,7 @@ public class HelloActivity extends Activity implements LocationListener, View.On
         mapButton = (Button) this.findViewById(R.id.button6);
         mapButton.setOnClickListener(this);
 
-
-        datenSammeln = false;
         positionen = new ArrayList<Location>();
-        gpxZeitFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
     }
 
     //Überprüfung ob GPS angeschaltet ist (&Internet)
@@ -131,60 +108,50 @@ public class HelloActivity extends Activity implements LocationListener, View.On
 
     //Start/Stopp/Speichern untereinander agieren können
     public void onClick(View v) {
-        if (v == startButton) {
-            startButton.setEnabled(false);
-            stoppButton.setEnabled(true);
-            speichernButton.setEnabled(false);
-            userlistButton.setEnabled(true);
-            clearButton.setEnabled(true);
-            datenSammeln = true;
-            sendGPSData();
-        }
-        else if (v == stoppButton) {
-            startButton.setEnabled(true);
-            stoppButton.setEnabled(false);
-            speichernButton.setEnabled(true);
-            userlistButton.setEnabled(true);
-            clearButton.setEnabled(true);
-            datenSammeln = false;}
-        else if (v == speichernButton) {
-            startButton.setEnabled(true);
-            stoppButton.setEnabled(false);
-            speichernButton.setEnabled(false);
-            userlistButton.setEnabled(true);
-            clearButton.setEnabled(true);
-            datenSammeln = false;
-            datenSpeichern();}
-       else if(v == userlistButton){
+
+       if(v == userlistButton){
             userlistButton.setEnabled(false);
-            startButton.setEnabled(true);
-            stoppButton.setEnabled(false);
-            speichernButton.setEnabled(false);
             datenSammeln = false;
-           String test =  getDaten();
+            String test =  getDaten();
             anzeigeUserlist.setText(test);
             clearButton.setEnabled(true);
             }
         else if(v == clearButton){
             clearButton.setEnabled(false);
-            startButton.setEnabled(true);
-            stoppButton.setEnabled(false);
-            speichernButton.setEnabled(false);
-            userlistButton.setEnabled(false);
-            datenSammeln = false;
+            userlistButton.setEnabled(true);
             anzeigeUserlist.setText("");
             }
-
-
         else if(v == mapButton && geodaten) {
-            startButton.setEnabled(true);
             datenSammeln = false;
-
+           //Erstellen der Map
+           LatLng User = new LatLng(lat, lng);
+           try {
+               if (googleMap == null) {
+                   LatLng User1 = new LatLng(9.1, 48.7);
+                   LatLng User2 = new LatLng(9.2, 48.77);
+                   String username = "user";
+                   googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+                   GoogleMapOptions options = new GoogleMapOptions();
+                   options.mapType(GoogleMap.MAP_TYPE_NORMAL);
+                   options.compassEnabled(false);
+                   googleMap.setMyLocationEnabled(true);
+                   googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(User, 15));
+                   if (googleMap == null) {
+                       Toast.makeText(getApplicationContext(), "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+                   }
+               }
+           }
+           catch (Exception e) {
+               e.printStackTrace();
+           }
+           maperstellt = true;
             //Layout-Wechsel
+            /*
             Intent nextScreen = new Intent( getApplicationContext(), Map.class );
             nextScreen.putExtra("lat", lat);
             nextScreen.putExtra("lng",lng);
             startActivity(nextScreen);
+            */
         }
     }
 
@@ -227,7 +194,7 @@ public class HelloActivity extends Activity implements LocationListener, View.On
         } catch (Exception e) {
             Log.e(TAG, "Error: " + e);
             e.printStackTrace(System.out);
-            Toast.makeText(this, "keine Verbindung zum Browser", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "keine Verbindung zum Browser", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -247,24 +214,29 @@ public class HelloActivity extends Activity implements LocationListener, View.On
     //Ermittlung der Breiten und Längengrade
     public void onLocationChanged(Location loc) {
 
-        Toast.makeText(this, "Standort wird ermittelt", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Standort wird ermittelt", Toast.LENGTH_SHORT).show();
 
         lng = loc.getLongitude();
         lat = loc.getLatitude();
 
-        // (Double.toString(laenge), Double.toString(breite));
-
         anzeigeBreite.setText(Location.convert(lat, Location.FORMAT_DEGREES));
         anzeigeLaenge.setText(Location.convert(lng, Location.FORMAT_DEGREES));
 
-        if (loc.hasAltitude()) {
-            anzeigeHoehe.setText(String.valueOf(loc.getAltitude()));
-        }
-        if (datenSammeln) {
-            positionen.add(loc);        //je nach dem speichert er die daten bzw. fügt sie der Liste hinzu welche später als Datei gespeichert werden kann
+        geodaten = true;
+        sendGPSData();
+
+        //Hier wird der eigene Marker erstellt
+        if(maperstellt){
+            //einlesen der daten aus jason format und speichern als
+            LatLng User = new LatLng(lat, lng);
+            String username = "User1";
+            if (marker != null) {
+              marker.remove();      //löscht immer nur einen marker
+            }
+            marker = googleMap.addMarker(new MarkerOptions().position(User).title(username).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).draggable(true)); //später sollten hier die anderen User hin
         }
 
-        geodaten = true;
+        // @ Jonas: Hier müssten die Geodaten + Username der anderen User ausgegeben werden und in den Marker gesetzt werden ähnlich wie oben, dadurch dass in in onlocationchanged wird es bereits immer wieder neu durchlaufen und aktuellen Daten abgefragt, deshalb wichtig nicht nur marker setzten sondern veraltete Standorte auch zu löschen
     }
 
     //Wenn GPS ausgeschaltet wird soll Meldung erscheinen
@@ -285,88 +257,6 @@ public class HelloActivity extends Activity implements LocationListener, View.On
         // TODO Auto-generated method stub
     }
 
-    // Dialog für das Speichern anzeigen und Daten anschließend speichern falls gewünscht
-    private void datenSpeichern() {
-        try {
-            final Dialog dialog = new Dialog(this);
-            dialog.setOwnerActivity(this);
-            dialog.setContentView(R.layout.save);
-
-            // Dialogelemente initialisieren
-            Resources res = getResources();
-            dialog.setTitle(res.getText(R.string.speichernDialogTitel));
-
-            final EditText dateiname = (EditText) dialog.findViewById(R.id.editTextDateiname);
-            dateiname.setText("meinStandort.gpx");
-
-            Button speichern = (Button) dialog.findViewById(R.id.speichernOK);
-            speichern.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Editable ed = dateiname.getText();
-
-                    try {
-                        positionenSchreiben(ed.toString());
-                        positionen.clear();
-                    } catch (Exception ex) {
-                        Log.d("carpelibrum", ex.getMessage());
-                    }
-                    dialog.dismiss();
-                }
-            });
-
-            dialog.show();
-
-        } catch (Exception ex) {
-            Log.d("carpelibrum", ex.getMessage());
-        }
-    }
-
-    // Schreibt die Daten auf die SD-Karte oder internen Speicher im GPX Format , @throws Exception
-    private void positionenSchreiben(String dateiName) throws Exception {
-        File sdKarte = Environment.getExternalStorageDirectory();
-        boolean sdKarteVorhanden = (sdKarte.exists() && sdKarte.canWrite());
-        File datei;
-
-        if (sdKarteVorhanden) {
-            datei = new File(sdKarte.getAbsolutePath() + File.separator + dateiName);
-        } else {
-            datei = new File(getFilesDir() + File.separator + dateiName);
-        }
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(datei));
-
-        // Dateikopf erstellen
-        writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>");
-        writer.newLine();
-        writer.write("<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" version=\"1.1\" ");
-        writer.write("creator=\"carpelibrum\" xmlns:xsi= \"http://www.w3.org/2001/XMLSchema-instance\" ");
-        writer.write("xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1/gpx.xsd\"");
-        writer.newLine();
-
-        // Daten schreiben
-        for (Location loc : positionen) {
-            locationSpeichern(loc, writer);
-        }
-
-        // Dateiende schreiben
-        writer.write("</gpx>");
-        writer.close();
-    }
-
-    // Übergebene Location in den gegebenen Writer schreiben @param loc @param writer
-    private void locationSpeichern(Location loc, BufferedWriter writer) throws IOException {
-        writer.write("<wpt lat=\"" + loc.getLatitude() + "\" lon=\"" + loc.getLongitude() + "\">");
-        writer.newLine();
-        writer.write("<ele>" + loc.getAltitude() + "</ele>");
-        writer.newLine();
-
-        String zeit = gpxZeitFormat.format(new Date(loc.getTime()));
-        writer.write("<time>" + zeit + "</time>");
-        writer.newLine();
-        writer.write("</wpt>");
-        writer.newLine();
-    }
-
     // Benutzer auffordern GPS zu aktivieren, beenden der Activity
     private void warnungUndBeenden() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -381,8 +271,5 @@ public class HelloActivity extends Activity implements LocationListener, View.On
         AlertDialog dialog = builder.create();
         dialog.show();
     }
-
-
-
 }
 
