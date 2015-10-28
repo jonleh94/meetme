a09a609e7743762b602a6fcdf31ba4661f0cafc0
Felix Schabernack <felix.schabernack@gmail.com>
2015-10-28 22:25:49 +0100

    ..

--- meetmeserver/src/main/java/de/dhbw/meetme/database/dao/GeoDao.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/database/dao/GeoDao.java
@@ -2,11 +2,16 @@ package de.dhbw.meetme.database.dao;
 
 
 import de.dhbw.meetme.domain.GeoData;
+import de.dhbw.meetme.domain.ScoreBoard;
 import de.dhbw.meetme.domain.User;
 import de.dhbw.meetme.domain.UuidId;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -19,6 +24,12 @@ public class GeoDao extends JpaDao<UuidId, GeoData> {
 
     public GeoDao() {
         super(GeoData.class);
+    }
+
+
+    public Collection<GeoData> listGeo() {
+        Query q = entityManager.createQuery("SELECT g FROM " + entityClass.getName() + " g");
+        return q.getResultList();
     }
 
 }
