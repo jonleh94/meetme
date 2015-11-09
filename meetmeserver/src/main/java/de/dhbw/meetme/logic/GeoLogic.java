d24fc75dcb0e75355cdfd3e7a5c3bb8d652de931
Felix Schabernack <felix.schabernack@gmail.com>
2015-11-09 18:44:54 +0100

    optimized code

--- meetmeserver/src/main/java/de/dhbw/meetme/logic/GeoLogic.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/logic/GeoLogic.java
@@ -1,16 +1,5 @@
 package de.dhbw.meetme.logic;
 
-import de.dhbw.meetme.database.Transaction;
-import de.dhbw.meetme.database.dao.GeoDao;
-import de.dhbw.meetme.domain.GeoData;
-import groovy.lang.Singleton;
-
-import javax.inject.Inject;
-
-
-/**
- * Created by schabi on 04.11.2015.
- */
 
 
 public class GeoLogic {
@@ -25,9 +14,12 @@ public class GeoLogic {
         double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
                 * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double dist = earthRadius * c;
+        return earthRadius * c;
 
-        return dist;
+
     }
+
+
+
 
 }
