89eea7f0a34cb7890d315bc3b5acc9322fb9dfc5
Felix Schabernack <felix.schabernack@gmail.com>
2015-11-10 19:55:02 +0100

    continued moving parts from the logic to GeoLogic

--- meetmeserver/src/main/java/de/dhbw/meetme/logic/GeoLogic.java
+++ meetmeserver/src/main/java/de/dhbw/meetme/logic/GeoLogic.java
@@ -1,8 +1,26 @@
 package de.dhbw.meetme.logic;
 
 
+import de.dhbw.meetme.database.dao.GeoDao;
+import de.dhbw.meetme.database.dao.UserDao;
+import de.dhbw.meetme.domain.GeoData;
+import de.dhbw.meetme.domain.User;
+import de.dhbw.meetme.servlet.UserServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 public class GeoLogic {
+
+    private static final Logger log = LoggerFactory.getLogger(GeoLogic.class);
+
+    @Inject
+    UserDao userDao;
+    @Inject
+    GeoDao geoDao;
+
 
     public static double getDistance(double lat1, double long1, double lat2, double long2) {
 
@@ -15,11 +33,38 @@ public class GeoLogic {
                 * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return earthRadius * c;
-
-
     }
 
 
+    public String postToGeo(String username, String password, double longitude, double latitude) {
+
+        boolean check = false;
+
+        GeoData thisgeoData = new GeoData();
+        User thisuser = userDao.findByUserName(username);
+
+        try {
+            if (thisuser.getPassword().equals(UserServlet.getMD5(password))) {
+                check = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        if (check) {
+            thisgeoData.setLatitude(latitude);   // Set Latitude from the URL command
+            thisgeoData.setLongitude(longitude); //Set Longitude from the URL command
+            thisgeoData.setUsername(username);
+            thisgeoData.setDate();
+            thisgeoData.setTeam(userDao.findByUserName(username).getTeam());
+            geoDao.persist(thisgeoData);
+        } else {
+            log.debug("COULD NOT UPDATE GEODATA: WRONG PASSWORD, please try again");
+            return "SERVER: WRONG PASSWORD, please try again";
+        }
+        return "SERVER: Operation successful, updated GeoData for User: " + username;
+    }
 
 
 }
